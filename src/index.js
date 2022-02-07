ff32ee8866de098ab3984b93d5c7974ce1ac374f
augusto <augustoantonelli88@gmail.com>
2022-02-07 17:00:23 -0300

    smal bug

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
-const os = require('os')
+//const os = require('os')
 
 
 const app = express();
 
 // SETINGS
 app.set('port', process.env.PORT || 3000);
-app.set('hostname', os.networkInterfaces()['Ethernet 2'][1]['address']); //get the ipv4 of the sistem
+//app.set('hostname', os.networkInterfaces()['Ethernet 2'][1]['address']); //get the ipv4 of the sistem
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs');
@@ -27,5 +27,5 @@ app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname, 'public')))
 
 // LISTENING THE SERVER
-app.listen(app.get('port'),app.get('hostname') , () => console.log(`Server runing at http://${app.get('hostname')}:${app.get('port')}/`));
-//app.listen(app.get('port') , () => console.log(`Server runing on port: ${app.get('port')}`));
+//app.listen(app.get('port'),app.get('hostname') , () => console.log(`Server runing at http://${app.get('hostname')}:${app.get('port')}/`));
+app.listen(app.get('port') , () => console.log(`Server runing on port: ${app.get('port')}`));
