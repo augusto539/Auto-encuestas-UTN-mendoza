577acf6c85b22ce3e9384e80e4efe04fd3653259
augusto <augustoantonelli88@gmail.com>
2022-02-16 19:56:37 -0300

    no longer use a second cookie for the number of surveys completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,10 +18,9 @@ router.get('/error/:error', (req, res) => {
   res.render('index.html', {title: ` - Error-${req.params.error}`, alert:'', error:req.params.error}); // render the index page with a error alert
 });
 // finish page
-router.get('/completadas/:legajo', (req, res) => {
+router.get('/completadas/:legajo/:materias_completadas', (req, res) => {
   let legajo_decrypted = e.decrypt('holasoyunacontrasenia',req.params.legajo); // decrypt the lagjo number
-  let cookie2 = req.cookies.data2
-  res.render('responce.html', {title: ' - Terminado', legajo:legajo_decrypted, materias:cookie2}); // show the finish page
+  res.render('responce.html', {title: ' - Terminado', legajo:legajo_decrypted, materias:req.params.materias_completadas}); // show the finish page
 });
 // POSTS
 // get information about the surveys to complete
@@ -44,8 +43,7 @@ router.post('/res/:legajo', (req,res) => {
   GP.encuesta(url_survey,data,req).then(() => {
     let legajo_encrypted = e.encrypt('holasoyunacontrasenia',req.params.legajo); // encrypt the lagjo number
     let materias_completadas = Object.keys(req.body).length - 1;
-    res.cookie('data2',materias_completadas);  // safe the data in a cookie
-    res.redirect('/completadas/' + legajo_encrypted); // redirect to the finish page
+    res.redirect(`/completadas/${legajo_encrypted}/${materias_completadas}`); // redirect to the finish page
   });
 });
 
